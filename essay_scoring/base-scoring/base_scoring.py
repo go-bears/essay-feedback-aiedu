c8c0968b99cacd5dcc7bc169f1029235e7f9e379
go-bears <melissa.fabros@gmail.com>
2025-04-06 23:37:11 -0700

    update prompt to accomodate anonymization

--- essay_scoring/base-scoring/base_scoring.py
+++ essay_scoring/base-scoring/base_scoring.py
@@ -43,11 +43,14 @@ You are a helpful essay assessement assistant that scores essays based on a rubr
 numerical score for the provided essay according to the specified rubric.
 
 Some guidelines are:
-- These essays were written by students ranging in grade levels from Grade 7 to Grade 10. Provide an appropriate holistic score for timed test conditions and grade level
-- The essay will be anonymized by replacing revealing details with all-caps tags starting with '@', such as '@ORGANIZATION1', '@CAPS2', and '@DATE1'. 
-- If information has been anonymized, do not penalize the essay if organization, coherence, clarity, and style are affected by anonymized information
+- These essays were written by students ranging in grade levels from Grade 7 to Grade 10.
+- Provide an appropriate holistic score for limited timed test conditions where there is litte to no time for revision
+- The essay has been anonymized by replacing revealing details with tags that start with '@' and all letters are capitalized, such as '@ORGANIZATION1', '@CAPS2', '@DATE1', and etc. 
+- If information has been anonymized, do not penalize the essay if organization, coherence, clarity, specificity, and style are affected.
+- You may make a reasonable substitution or interpolation for the anonymized information to preserve minimal coherence and readability, 
+  but do not change or edit the original essay with the substitution.
 
-The rubric or rubrics for this question is as follows:
+The rubric or rubrics for this essay is as follows:
 {rubric}
 
 The prompt is as follows:
@@ -56,11 +59,11 @@ The prompt is as follows:
 
 essay_prompt = """
 
-The essay to score is as follows:
+Review the given rubric and prompt carefully. The essay that requires a holistic score from the rubric is as follows:
 
 {essay_text}
 
-Only provide a domain 1 score using the provided rubric.
+Provide a numerical domain_1_score by using the provided rubric's guidance.
 Output the score in JSON using the following format:
 {{
     "essay_id": "{essay_id}",
@@ -73,15 +76,15 @@ Output the score in JSON using the following format:
 essay_set_2_essay_prompt = """
 This essay requires 2 scores, and you have been provided with both rubrics in the system prompt.
 
-Please provide a score for each domain.
+Please provide a numerical score for each domain based on the appropriate rubric.
 Domain 1: Writing Applications
 Domain 2: Language Conventions
 
-The essay to score is as follows:
+Review the given rubrics and prompt carefully. The essay that requires a holistic score from the rubric is as follows:
 
 {essay_text}
 
-Output the score in JSON using the following format:
+Output the scores in JSON using the following format:
 {{
     "essay_id": {essay_id},
     "comments": {commentary},
